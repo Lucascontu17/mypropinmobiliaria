fc8154de17445595dfa4ad3818eb63ffb814af87
Lucas contureyuzon <luconture@gmail.com>
2026-05-29 10:45:24 -0300

    fix

--- src/pages/cobranzas/CobranzasPage.tsx
+++ src/pages/cobranzas/CobranzasPage.tsx
@@ -50,7 +50,8 @@ export function CobranzasPage() {
             // @ts-ignore
             if (data?.periodo_activo && !periodoIniciado) {
               // @ts-ignore
-              setPeriodoActual(data.periodo_activo.trim());
+              const nuevoPeriodo = data.periodo_activo.trim();
+              setPeriodoActual(nuevoPeriodo);
               setPeriodoIniciado(true);
             }
         }
