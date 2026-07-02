2b81cc725535339d4d0c9f395471865409f71120
Lucas contureyuzon <luconture@gmail.com>
2026-07-02 10:57:26 -0300

    feat: agrega tipos Galpon y Oficina al formulario de propiedades

--- src/components/propiedades/PropertyForm.tsx
+++ src/components/propiedades/PropertyForm.tsx
@@ -367,6 +367,8 @@ export function PropertyForm({ initialData, owners, tenantId, onSubmitSuccess, o
                   <option value="casa">🏡 Casa</option>
                   <option value="ph">🏘️ PH</option>
                   <option value="local">🏬 Local</option>
+                  <option value="galpon">🏭 Galpón</option>
+                  <option value="oficina">🏢 Oficina</option>
                   <option value="terreno">🌱 Terreno</option>
                   <option value="habitacion">🛌 Habitación</option>
                   <option value="otro">❓ Otro</option>
